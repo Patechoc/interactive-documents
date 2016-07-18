c63137e3451e1db6815cff9a09ca551656726dc3
Hans Petter Langtangen <hpl@simula.no>
2016-07-18 06:31:39 -0700

    Debugged code. Made officiial_demo

--- examples/embed_multiple/makedo.sh
+++ examples/embed_multiple/makedo.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -x
 name=template
-name=tmp
+doconce clean
 bokeh serve --allow-websocket-origin=localhost:8000 &
+sleep 8  # wait for bokeh serve
 doconce format html $name &
+doconce split_html $name --nav_button=text --method=split
 python -m SimpleHTTPServer &
 
 echo "Navigate to: http://localhost:8000/$name.html"
